4825100f645741c5ae31367648fbe139e4d9e524
anastasia535 <casovskaaanastasia1@gmail.com>
2025-09-04 21:55:26 +0300

    Initial commit

---
+++ src/Lab1_Variant20_Group_Chasovskaya.java
@@ -0,0 +1,22 @@
+public class Lab1_Variant20_Group_Chasovskaya {
+    public static void main(String[] args) {
+        //первое
+        double pounds = 15.5;
+        double kilograms = pounds * 0.4536;
+        System.out.println("Первое задание: \n15.5 фунтов = " + kilograms + " кг");
+        System.out.println();
+
+        //второе
+        String easternZodiac = "Свинья";
+        System.out.println("Второе задание: \nМой знак по восточному календарю - " + easternZodiac);
+        System.out.println();
+
+        //третье
+        int number1 = -15;
+        int number2 = 25;
+        int absValue = Math.abs(number1);
+        int oppositeValue = -number2;
+        System.out.println("Третье задание: \nМодуль числа -15: " + absValue +
+                "\nПротивоположное число 25: " + oppositeValue);
+    }
+}
